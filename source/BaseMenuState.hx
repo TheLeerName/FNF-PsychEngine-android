5989c618ca23736a6eb10e6bbd4c2a44d62a1900
TheLeerName <lesha.nechaev.05@mail.ru>
2023-07-17 22:09:52 +0700

    will extend all menus by it

---
+++ source/BaseMenuState.hx
@@ -0,0 +1,83 @@
+package;
+
+import openfl.Lib;
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.group.FlxGroup;
+
+import input.AttachedHitbox;
+
+/**
+ * Used for easy adding menus
+ */
+class BaseMenuState<T:FlxObject> extends MusicBeatState {
+	/**
+	 * Current selected index on `menuItems`
+	 */
+	public var curSelected(default, set):Int = 0;
+	/**
+	 * Group with items for menu
+	 */
+	public var menuItems:FlxTypedGroup<T> = new FlxTypedGroup<T>();
+	/**
+	 * Calls on changed `curSelected`. You don't need do `super.changeSelection(value);` cuz it empty
+	 * @param value Current selected index
+	 */
+	public function changeSelection(value:Int) {}
+	/**
+	 * Calls on `controls.ACCEPT`. You don't need do `super.accept();` cuz it empty
+	 */
+	public function accept() {}
+	/**
+	 * Calls on `controls.BACK`. You don't need do `super.back();` cuz it empty
+	 */
+	public function back() {}
+
+	/**
+	 * For tapping on selected object by pointer. To enable/disable, change `acceptHitbox.visible`. Works only in mobile!
+	 */
+	public var acceptHitbox:AttachedHitbox<T> = new AttachedHitbox<T>(null, 'accept');
+
+	/**
+	 * Make sure you adding objects above `super.create();`
+	 */
+	override function create() {
+
+		add(menuItems);
+
+		acceptHitbox.visible = controls.mobileInput;
+		add(acceptHitbox);
+
+		super.create();
+
+		if (FlxG.save.data.states_curSelected == null)
+			FlxG.save.data.states_curSelected = {};
+		var value:Null<Int> = Reflect.getProperty(FlxG.save.data.states_curSelected, Type.getClassName(Type.getClass(this)));
+		curSelected = value != null ? value : 0;
+
+		Lib.application.window.onClose.add(destroy); // why it not calls on close app????????????????????????
+	}
+
+	override function update(elapsed:Float) {
+		super.update(elapsed);
+
+		if (controls.BACK) back();
+		if (controls.ACCEPT) accept();
+		if (controls.UI_UP_P) curSelected--;
+		if (controls.UI_DOWN_P) curSelected++;
+	}
+
+	override function destroy() {
+		Reflect.setProperty(FlxG.save.data.states_curSelected, Type.getClassName(Type.getClass(this)), curSelected);
+		FlxG.save.flush();
+
+		super.destroy();
+	}
+
+	function set_curSelected(v:Int):Int {
+		v = Math.floor(Math.min(Math.max(v, 0), menuItems.length - 1));
+		changeSelection(v);
+		if (menuItems.length > 0) acceptHitbox.sprTracker = menuItems.members[v];
+		return curSelected = v;
+	}
+}
